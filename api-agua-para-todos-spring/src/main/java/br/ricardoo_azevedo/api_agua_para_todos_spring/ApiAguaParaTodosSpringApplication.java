c8ea7bebb6f447cca2fd6243308e8ab2df114526
ricardoo-azevedo <ricardoazevedo1911@gmail.com>
2025-11-17 16:22:12 -0300

    feat: inicialização do projeto

---
+++ api-agua-para-todos-spring/src/main/java/br/ricardoo_azevedo/api_agua_para_todos_spring/ApiAguaParaTodosSpringApplication.java
@@ -0,0 +1,13 @@
+package br.ricardoo_azevedo.api_agua_para_todos_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAguaParaTodosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAguaParaTodosSpringApplication.class, args);
+	}
+
+}
---
+++ api-agua-para-todos-spring/src/test/java/br/ricardoo_azevedo/api_agua_para_todos_spring/ApiAguaParaTodosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ricardoo_azevedo.api_agua_para_todos_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAguaParaTodosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
