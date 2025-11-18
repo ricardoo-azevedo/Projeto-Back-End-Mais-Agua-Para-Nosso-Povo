b7003a4ec14fe7a51a088512a6d197cfbdd5942f
ricardoo-azevedo <ricardoazevedo1911@gmail.com>
2025-11-18 01:16:57 -0300

    feat: adicionando a camada de interface de service e a de implementação de familia

---
+++ api-agua-para-todos-spring/src/main/java/br/ricardoo_azevedo/api_agua_para_todos_spring/service/impls/FamiliaServiceImpl.java
@@ -0,0 +1,121 @@
+package br.ricardoo_azevedo.api_agua_para_todos_spring.service.impls;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.ricardoo_azevedo.api_agua_para_todos_spring.dtos.FamiliaDto;
+import br.ricardoo_azevedo.api_agua_para_todos_spring.models.Familia;
+import br.ricardoo_azevedo.api_agua_para_todos_spring.repositorys.FamiliaRepository;
+import br.ricardoo_azevedo.api_agua_para_todos_spring.service.interfaces.FamiliaServiceInterface;
+
+@Service
+public class FamiliaServiceImpl implements FamiliaServiceInterface {
+
+
+  @Autowired
+  FamiliaRepository familiaRepository;
+
+    public FamiliaDto toDto (Familia familia){
+        return new FamiliaDto(
+          familia.getId(), 
+          familia.getEndereco(), 
+          familia.isPossui_captacao_calhas(),
+          familia.getEntrega(), 
+          familia.getProxima_previsao(), 
+          familia.getLatitude(),
+          familia.getLongitude());
+    }
+
+
+  @Override
+  public FamiliaDto salvar(FamiliaDto familiaDto) {
+    Familia familia = new Familia();
+    // if (familiaRepository.existsById(familiaDto.getId() {
+    // throw new Exception();
+    // }
+    familia.setEndereco(familiaDto.getEndereco());
+    familia.setPossui_captacao_calhas(familiaDto.isPossui_captacao_calhas());
+    familia.setEntrega(familiaDto.getEntrega());
+    familia.setProxima_previsao(familiaDto.getProxima_previsao());
+    familia.setLatitude(familiaDto.getLatitude());
+    familia.setLongitude(familiaDto.getLongitude());
+
+    Familia familiaSalva = familiaRepository.save(familia);
+
+    return new FamiliaDto(
+      familiaSalva.getId(), 
+      familiaSalva.getEndereco(), 
+      familiaSalva.isPossui_captacao_calhas(),
+      familiaSalva.getEntrega(), 
+      familiaSalva.getProxima_previsao(), 
+      familiaSalva.getLatitude(),
+      familiaSalva.getLongitude());
+  }
+
+  @Override
+  public FamiliaDto editarPorId(FamiliaDto familiaDto, UUID id) {
+    // if(id == null){
+    //   throw new expcetion;
+    // }
+    //depois vou criar a exception personalizada e consertar essas verificações malditas
+    Familia familia = familiaRepository.findById(id).orElseThrow(() -> new RuntimeException());
+
+    familia.setEndereco(familiaDto.getEndereco());
+    familia.setPossui_captacao_calhas(familiaDto.isPossui_captacao_calhas());
+    familia.setEntrega(familiaDto.getEntrega());
+    familia.setProxima_previsao(familiaDto.getProxima_previsao());
+    familia.setLatitude(familiaDto.getLatitude());
+    familia.setLongitude(familiaDto.getLongitude());
+    Familia familiaEditada = familiaRepository.save(familia);
+
+    return new FamiliaDto(
+      familiaEditada.getId(), 
+      familiaEditada.getEndereco(), 
+      familiaEditada.isPossui_captacao_calhas(),
+      familiaEditada.getEntrega(), 
+      familiaEditada.getProxima_previsao(), 
+      familiaEditada.getLatitude(),
+      familiaEditada.getLongitude());
+  }
+
+  @Override
+  public List<FamiliaDto> listar() {
+   List<Familia> familias = familiaRepository.findAll(); 
+   return familias.stream()
+   .map(this::toDto).collect(Collectors.toList());
+
+  }
+
+  @Override
+  public FamiliaDto pesquisarPorId(UUID id) {
+    // if(id == null){
+    //   throw new expcetion;
+    // }
+    return familiaRepository.findById(id)
+    .map(this::toDto).orElseThrow(() -> new RuntimeException("Achou nao"));
+  }
+
+  @Override
+  public List<FamiliaDto> pesquisarPorEndereco(String endereco) {
+    List<Familia> resultados = familiaRepository.findByEnderecoContaining(endereco);
+    return resultados.stream().map(this::toDto).collect(Collectors.toList());
+  }
+
+  @Override
+  public void deletarPorId(UUID id) {
+   /* if (id == null) {
+            throw new IdInvalidoException("O id é nulo");
+        }
+        if (familiaRepository.existsById(id) == false) {
+            throw new EscolaNaoEncontradaException();
+        } */
+        familiaRepository.deleteById(id);
+    }
+  
+  }
+
+
---
+++ api-agua-para-todos-spring/src/main/java/br/ricardoo_azevedo/api_agua_para_todos_spring/service/interfaces/FamiliaServiceInterface.java
@@ -0,0 +1,22 @@
+package br.ricardoo_azevedo.api_agua_para_todos_spring.service.interfaces;
+
+import java.util.List;
+import java.util.UUID;
+
+import br.ricardoo_azevedo.api_agua_para_todos_spring.dtos.FamiliaDto;
+
+public interface FamiliaServiceInterface {
+
+  FamiliaDto salvar (FamiliaDto familiaDto);
+
+  FamiliaDto editarPorId (FamiliaDto familiaDto, UUID id);
+
+  List<FamiliaDto> listar();
+
+  FamiliaDto pesquisarPorId (UUID id);
+
+  List<FamiliaDto> pesquisarPorEndereco(String endereco);
+
+  void deletarPorId(UUID id);
+
+}
