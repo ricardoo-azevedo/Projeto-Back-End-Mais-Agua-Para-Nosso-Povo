dd0ed505e3f0d86b9b81d05e01598999f5e1305b
ricardoo-azevedo <ricardoazevedo1911@gmail.com>
2025-11-18 18:00:05 -0300

    feat: mudando swag

--- docs/dist/swagger-initializer.js
+++ docs/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://raw.githubusercontent.com/ricardoo-azevedo/Projeto-Back-End-Mais-Agua-Para-Nosso-Povo/main/docs/open-api.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
