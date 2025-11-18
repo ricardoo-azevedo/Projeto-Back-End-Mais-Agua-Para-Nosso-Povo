8377f275bf8f626824b531a9ca160af098e3a4bb
ricardoo-azevedo <ricardoazevedo1911@gmail.com>
2025-11-18 01:15:11 -0300

    feat: adicionando interfaces que extende a repositorys de familia

---
+++ api-agua-para-todos-spring/src/main/java/br/ricardoo_azevedo/api_agua_para_todos_spring/repositorys/FamiliaRepository.java
@@ -0,0 +1,21 @@
+package br.ricardoo_azevedo.api_agua_para_todos_spring.repositorys;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.ricardoo_azevedo.api_agua_para_todos_spring.models.Familia;
+
+@Repository
+public interface FamiliaRepository extends JpaRepository<Familia, UUID> {
+
+  List<Familia> findByEndereco(String endereco);
+
+  //cheacar isso no model ein
+  boolean existsByEndereco(String endereco);
+
+  List<Familia> findByEnderecoContaining(String nome);
+
+}
