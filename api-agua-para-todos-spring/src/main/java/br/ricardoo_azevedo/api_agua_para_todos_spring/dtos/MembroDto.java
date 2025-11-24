afdba5da0b8818e4dd1fa37d1219c9ab7e4e4bd9
ricardoo-azevedo <ricardoazevedo1911@gmail.com>
2025-11-24 17:43:00 -0300

    .

--- api-agua-para-todos-spring/src/main/java/br/ricardoo_azevedo/api_agua_para_todos_spring/dtos/MembroDto.java
+++ api-agua-para-todos-spring/src/main/java/br/ricardoo_azevedo/api_agua_para_todos_spring/dtos/MembroDto.java
@@ -4,7 +4,6 @@ import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
