7ce14fd46a659e93e9315b97d8e460482d21b46b
ricardoo-azevedo <ricardoazevedo1911@gmail.com>
2025-11-27 11:34:15 -0300

    feat: adicionando as classes de entidade e dto de Distribuição

---
+++ api-agua-para-todos-spring/src/main/java/br/ricardoo_azevedo/api_agua_para_todos_spring/dtos/DistribuicaoDto.java
@@ -0,0 +1,42 @@
+package br.ricardoo_azevedo.api_agua_para_todos_spring.dtos;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString @Getter @Setter
+public class DistribuicaoDto {
+
+    UUID id;
+
+    public DistribuicaoDto(LocalDate dataEntrega, int quantidadeLitros, LocalDate previsaoProxima, String observacoes){
+        this.dataEntrega = dataEntrega;
+        this.quantidadeLitros = quantidadeLitros;
+        this.previsaoProxima = previsaoProxima;
+        this.observacoes = observacoes;
+
+    }
+
+    @NotNull(message = "Data entrega nao deve ser nulo")
+    LocalDate dataEntrega; 
+   
+    @NotNull(message = "Quantidade de litros nao deve ser nulo")
+    int quantidadeLitros;
+
+    @NotNull(message = "Previsao proxima nao deve ser nulo")
+    LocalDate previsaoProxima;
+   
+    @Size(max = 500, message = "Numero maximo de caracteres")
+    String observacoes;
+
+
+}
---
+++ api-agua-para-todos-spring/src/main/java/br/ricardoo_azevedo/api_agua_para_todos_spring/models/Distribuicao.java
@@ -0,0 +1,52 @@
+package br.ricardoo_azevedo.api_agua_para_todos_spring.models;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter @ToString
+@Table(name = "TB_Distribuicao")
+public class Distribuicao {
+
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "BINARY(16)")
+    UUID id;
+
+    public Distribuicao(LocalDate dataEntrega, int quantidadeLitros, LocalDate previsaoProxima, String observacoes){
+        this.dataEntrega = dataEntrega;
+        this.quantidadeLitros = quantidadeLitros;
+        this.previsaoProxima = previsaoProxima;
+        this.observacoes = observacoes;
+
+    }
+
+    @Column(nullable = false)
+    LocalDate dataEntrega; 
+    
+    @Column(nullable = false)
+    int quantidadeLitros;
+
+    @Column(nullable = false)
+    LocalDate previsaoProxima;
+
+    @Column(nullable = true)
+    String observacoes;
+
+}
