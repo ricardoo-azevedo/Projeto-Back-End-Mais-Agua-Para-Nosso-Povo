c867878792d26de5149d51f4d4f01876081f9981
ricardoo-azevedo <ricardoazevedo1911@gmail.com>
2025-11-27 11:32:20 -0300

    feat: adicionando a anotação referente a Repository em CisternaRepository

--- api-agua-para-todos-spring/src/main/java/br/ricardoo_azevedo/api_agua_para_todos_spring/repositorys/CisternaRepository.java
+++ api-agua-para-todos-spring/src/main/java/br/ricardoo_azevedo/api_agua_para_todos_spring/repositorys/CisternaRepository.java
@@ -3,9 +3,11 @@ package br.ricardoo_azevedo.api_agua_para_todos_spring.repositorys;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.ricardoo_azevedo.api_agua_para_todos_spring.models.Cisterna;
 
+@Repository
 public interface CisternaRepository extends JpaRepository<Cisterna, UUID> {
     
 }
