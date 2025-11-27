d31702a6047fe2efa9a5641525ebee6ffd67ce3e
ricardoo-azevedo <ricardoazevedo1911@gmail.com>
2025-11-27 12:02:08 -0300

    feat: adicionando o corpo dos metodos herdados de ServiceInterface na camada de ServiceImpl de Distribuição

--- api-agua-para-todos-spring/src/main/java/br/ricardoo_azevedo/api_agua_para_todos_spring/service/impls/DistribuicaoServiceImpl.java
+++ api-agua-para-todos-spring/src/main/java/br/ricardoo_azevedo/api_agua_para_todos_spring/service/impls/DistribuicaoServiceImpl.java
@@ -2,36 +2,62 @@ package br.ricardoo_azevedo.api_agua_para_todos_spring.service.impls;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.ricardoo_azevedo.api_agua_para_todos_spring.dtos.DistribuicaoDto;
+import br.ricardoo_azevedo.api_agua_para_todos_spring.models.Distribuicao;
 import br.ricardoo_azevedo.api_agua_para_todos_spring.repositorys.DistribuicaoRepository;
 import br.ricardoo_azevedo.api_agua_para_todos_spring.service.interfaces.DistribuicaoServiceInterface;
 
 @Service
 public class DistribuicaoServiceImpl implements DistribuicaoServiceInterface{
 
+
     @Autowired
     private DistribuicaoRepository distribuicaoRepository;
 
+
+    public Distribuicao toEntity (DistribuicaoDto distribuicaoDto){
+        return new Distribuicao(
+            distribuicaoDto.getDataEntrega(), 
+            distribuicaoDto.getQuantidadeLitros(), 
+            distribuicaoDto.getPrevisaoProxima(), 
+            distribuicaoDto.getObservacoes());
+    }
+
+    public DistribuicaoDto toDto (Distribuicao distribuicao){
+        return new DistribuicaoDto(
+            distribuicao.getId(), 
+            distribuicao.getDataEntrega(), 
+            distribuicao.getQuantidadeLitros(), 
+            distribuicao.getPrevisaoProxima(), 
+            distribuicao.getObservacoes());
+    }
+
     @Override
     public DistribuicaoDto salvar(DistribuicaoDto distribuicaoDto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'salvar'");
+        Distribuicao distribuicaoSalva = distribuicaoRepository.save(toEntity(distribuicaoDto));
+        return toDto(distribuicaoSalva);
     }
 
     @Override
     public List<DistribuicaoDto> listar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listar'");
+        List<Distribuicao> distribuicoes = distribuicaoRepository.findAll();
+        return distribuicoes.stream()
+        .map(this::toDto).collect(Collectors.toList());
     }
 
     @Override
     public void deletarPorId(UUID id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deletarPorId'");
+        if (id == null){
+            throw new RuntimeException("O id é nulo");
+        } if (!distribuicaoRepository.existsById(id)){
+            throw new RuntimeException("Distribuição nao encontrada");
+        }
+        distribuicaoRepository.deleteById(id);
     }
 
 }
